3fdb496ec8f852007a59fd5d47547bf5fe1a897d
tunggulalmajid <tunggulmajid@gmail.com>
2025-05-03 00:03:37 +0700

    loadingPage

--- lib/loading_page.dart
+++ lib/loading_page.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class LoadingPage extends StatefulWidget {
   @override
-  _LoadingPageState createState() => _LoadingPageState();
+  LoadingPageState createState() => LoadingPageState();
 }
 
-class _LoadingPageState extends State<LoadingPage> {
+class LoadingPageState extends State<LoadingPage> {
   @override
   void initState() {
     super.initState();
@@ -19,23 +19,44 @@ class _LoadingPageState extends State<LoadingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red, // Sesuai desain latar merah
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment.center,
+            radius: 0.8,
+            colors: [Color(0xFFEE3343), Color(0xFF881D26)],
+            stops: [0.0, 1.0],
+          ),
+        ),
+        child: Stack(
           children: [
-            Icon(Icons.health_and_safety, size: 80, color: Colors.white),
-            SizedBox(height: 16),
-            Text(
-              'Healthy Heroes',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+            Positioned.fill(
+              child: Image.asset(
+                'assets/BackgroundAwanLoad.png',
+                fit: BoxFit.cover,
               ),
             ),
-            SizedBox(height: 24),
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/LogoHealth.png',
+                    fit: BoxFit.fill,
+                    
+                  ),
+
+                  Image.asset(
+                    'assets/hhtext.png',
+                    fit: BoxFit.cover,
+                    width: 500,
+                  ),
+                  SizedBox(height: 24),
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
